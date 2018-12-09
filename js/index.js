a9d39a7aa1f25d22fd8f722726e1c6188393b51d
Josh <josh0661@kea.stud.dk>
2018-12-09 23:15:31 +0100

    Added on scroll jQuery function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
-testetst;
+// Stick Navbar jQuery Script //
+$(window).on("scroll", function() {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("black");
+  } else {
+    $("nav").removeClass("black");
+  }
+});
