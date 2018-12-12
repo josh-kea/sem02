69abaf8cef9e4f4c85e8710bec66fb04c9e7837e
Josh <josh0661@kea.stud.dk>
2018-12-12 16:37:51 +0100

    LoadAll included

--- js/index.js
+++ js/index.js
@@ -8,3 +8,11 @@ $(window).on("scroll", function() {
 });
 
 // Rest Api //
+const baseLink = "http://joshkap.com/kimrichardt/wp-json/wp/v2";
+
+// Function, called later, to load all of the products post type in wordpress backend //
+function loadAll() {
+  fetch(baseLink + "product?&_embed")
+    .then(e => e.json())
+    .then(showAll);
+}
